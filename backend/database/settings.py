6fe69c6e838e9209209a2e5084bfe1659ce79dd1
Prakruti-Bhavsar <21104067.prakruti.bhavsar@gmail.com>
2025-01-29 22:20:32 +0530

    backend setup

--- backend/database/settings.py
+++ backend/database/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'database.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "ProjectSpace",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432",
+
     }
 }
 
